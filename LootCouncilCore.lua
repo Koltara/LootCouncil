2f2c55d8a1da029e39b782a87fc88832f73b85b0
Colin Ross <cross1@ltu.edu>
2016-07-26 14:27:23 -0400

    Baseline Tabs for UI

--- LootCouncilCore.lua
+++ LootCouncilCore.lua
@@ -20,6 +20,97 @@ local defaults =
   	message = "LootCouncil Defaults",
  	},
 }
+local function DrawViewGroup(container)
+	local desc = AceGUI:Create("Label")
+
+	desc:SetText("View")
+	desc:SetFullWidth(true)
+
+	container:AddChild(desc)
+
+
+end
+
+local function DrawUpdateGroup(container)
+
+	local toBeUpdated
+	local amount
+
+	--Tab Label
+	local desc = AceGUI:Create("Label")
+	desc:SetText("Update")
+	desc:SetFullWidth(true)
+	container:AddChild(desc)
+
+	--Name Enter Box
+	local nameBox = AceGUI:Create("EditBox")
+	nameBox:SetLabel("Enter name of Raider in system: ")
+	nameBox:SetWidth(200)
+	nameBox:SetCallback("OnEnterPressed", function(widget, event, text) toBeUpdated = text end)
+	container:AddChild(nameBox)
+
+	local amountBox = AceGUI:Create("EditBox")
+	amountBox:SetLabel("Amount:")
+	amountBox:SetWidth(200)
+	amountBox:SetNumeric(true)
+	amountBox:SetNumber(0)
+	amountBox:SetCallback("OnEnterPressed", function(widget, event, text) amount = text end)
+	container:AddChild(amountBox)
+
+	local addButton = AceGUI:Create("Button")
+	addButton:SetWidth(50)
+	addButton:SetText("+")
+	addButton:SetCallback("OnClick", AddPointsToMember(toBeUpdated, amount))
+	container:AddChild(addButton)
+
+	local subButton = AceGUI:Create("Button")
+	addButton:SetWidth(50)
+	addButton:SetText("-")
+	addButton:SetCallback("OnClick", SubPointsFromMember(toBeUpdated, amount))
+	container:AddChild(addButton)
+
+
+end
+
+local function AddPointsToMember(member, amount)
+	-- Add points to the raid member's total
+end
+local function SubPointsFromMember(member, amount)
+	-- Subtract points from the raid member's total
+end
+
+local function DrawRaidGroup(container)
+	local desc = AceGUI:Create("Label")
+
+	desc:SetText("Raid")
+	desc:SetFullWidth(true)
+	container:AddChild(desc)
+
+	local raidEndButton = AceGUI:Create("Button")
+	raidEndButton:SetWidth(200)
+	raidEndButton:SetText("End Raid")
+	raidEndButton:SetCallback("OnClick", "EndRaid")
+
+	local raidStartButton = AceGUI:Create("Button")
+	raidStartButton:SetWidth(200)
+	raidStartButton:SetText("Start Raid")
+	raidStartButton:SetCallback("OnClick", "StartRaid")
+
+	container:AddChild(raidStartButton)
+	container:AddChild(raidEndButton)
+end
+
+
+local function SelectTab(container, event, group)
+	container:ReleaseChildren()
+	if group == "viewTab" then
+		DrawViewGroup(container)
+	elseif group == "raidTab" then
+		DrawRaidGroup(container)
+	elseif group == "updateTab" then
+		DrawUpdateGroup(container)
+	end
+end
 
 function LootCouncil:OnInitialize()
 
@@ -27,20 +118,16 @@ function LootCouncil:OnInitialize()
 	mainWindow:SetTitle("Loot Council")
 	mainWindow:SetStatusText("Loot Council Main Window")
 	mainWindow:SetCallback("OnClose", function(widget) AceGUI:Release(widget) end)
-	mainWindow:SetLayout("Flow")
+	mainWindow:SetLayout("Fill")
 
-	raidStartButton = AceGUI:Create("Button")
-	raidStartButton:SetWidth(200)
-	raidStartButton:SetText("Start Raid")
-	raidStartButton:SetCallback("OnClick", "StartRaid")
+	local tab = AceGUI:Create("TabGroup")
+	tab:SetLayout("Flow")
+	tab:SetTabs({{text="View", value="viewTab"}, {text="Raid", value = "raidTab"}, {text="Update", value = "updateTab"}})
+	tab:SetCallback("OnGroupSelected", SelectGroup)
+	tab:SelectTab("View")
 
-	raidEndButton = AceGUI:Create("Button")
-	raidEndButton:SetWidth(200)
-	raidEndButton:SetText("End Raid")
-	raidEndButton:SetCallback("OnClick", "EndRaid")
+	mainWindow:AddChild(tab)
 
-	mainWindow:AddChild(raidEndButton)
-	mainWindow:AddChild(raidStartButton)
 
 	self.db = LibStub("AceDB-3.0"):New("LootCouncilDB", defaults, true)
 
